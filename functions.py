a04b04945bf0bc6bc652a876c2f8956ea33c8d10
IdanMalihi <81778053+TraiLeR2@users.noreply.github.com>
2022-05-06 22:46:41 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,18 +1,7 @@
 import pyfiglet
-import os
+
+enter = "\n"
 
 def banner():
     banner = pyfiglet.figlet_format("WireAttack")
     return banner
-
-def menu():
-    print("""Menu:
-    1) Enable Monitor Mode
-    2) Disable Monitor Mode
-    3) Installing Tools
-    4) update system""")
-
-    option = input("\nChoice > ")
-
-    if option == 4:
-        os.system("sudo apt-get update")
