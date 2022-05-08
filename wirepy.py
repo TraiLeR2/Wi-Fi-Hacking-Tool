4de9d8c530046ef59841f7f28adf82986b820c9a
IdanMalihi <81778053+TraiLeR2@users.noreply.github.com>
2022-05-08 23:39:52 +0300

    Update wirepy.py

--- wirepy.py
+++ wirepy.py
@@ -1,14 +1,16 @@
 import os
 import time
 from functions import banner
+from termcolor import colored
 
 enter = "\n"
 
 def menu():
     os.system("clear")
 
-    print("""{0}
-                *** The Script was Created By Idan Malihi ***{1}""".format(banner(), enter))
+    print(colored(f"{banner()}", "red"))
+
+    print("*** The Script was Created By TraiLeR (Idan) ***{0}".format(enter))
 
     print("""Menu:
     (1) Enable Monitor Mode
@@ -18,15 +20,17 @@ def menu():
     (5) Update & Upgrade Machine
     (6) Install wireless tools (Type 5 before)
     (7) Crack Wi-Fi with rockyou.txt
-    (8) About the Hacker
-    (9) Exit""")
+    (8) Crack Wi-Fi with wordlist (Any)
+    (9) Create a wordlist (Crunch Needed)
+    (10) About the Hacker
+    (11) Exit""")
 
     option = int(input("{0}Choice > ".format(enter)))
 
-    if option == 9:
+    if option == 11:
         exit()
 
-    elif option == 8:
+    elif option == 10:
         print("""
 Hi, my name is Idan (TraiLeR) and I am an Ethical Hacker, Bug Bounty Hunter and Cyber Security Researcher.
 Since I was a child I loved challenges and do things that would open my mind and be more creative.
@@ -80,15 +84,16 @@ Feel free to reach me on Telegram:
     elif option == 6:
         os.system("clear")
         print("""
-1) Aircrack-ng
-2) Wifite
-3) Kismet
-4) Wifiphisher
-5) inSSIDer
-6) Wireshark
-7) Airgeddon
-8) Wifi Honey
-9) Install ALL
+(1) Aircrack-ng
+(2) Wifite
+(3) Kismet
+(4) Wifiphisher
+(5) inSSIDer
+(6) Wireshark
+(7) Airgeddon
+(8) Wifi Honey
+(9) Crunch
+(10) Install ALL
             """)
 
         tool = int(input("Enter the number of the tool > "))
@@ -119,18 +124,22 @@ Feel free to reach me on Telegram:
                 os.system("sudo apt-get update && sudo apt-get install wifitap")
 
             elif tool == 9:
+                os.system("sudo apt-get update && sudo apt-get install -y crunch")
+
+            elif tool == 10:
                 os.system("sudo apt-get update && sudo apt-get install -y aircrack-ng wifite kismet wifiphisher linssid wireshark airgeddon wifitap")
 
         except:
             print("You did not type the right number on the list!")
 
-    elif option == 6:
+    elif option == 7:
         if os.path.exists("/usr/share/wordlists/rockyou.txt"):
             handshake = str(input("Enter the path of the handshake file > "))
             print("{0}To exit Press CTRL +C".format(enter))
             os.system("aircrack-ng {0} -w /usr/share/wordlists/rockyou.txt".format(handshake))
             time.sleep(5)
             exit()
+
         elif not os.path.exists("/usr/share/wordlists/rockyou.txt"):
             os.system("gzip -d /usr/share/wordlists/rockyou.txt.gz")
             handshake = str(input("Enter the path of the handshake file > "))
@@ -138,5 +147,50 @@ Feel free to reach me on Telegram:
             os.system("aircrack-ng {0} -w /usr/share/wordlists/rockyou.txt".format(handshake))
             time.sleep(5)
             exit()
+
+    elif option == 8:
+        handshake = str(input("Enter the path of the handshake file > "))
+        wordlist = str(input("Enter the path of the wordlist file > "))
+        os.system("aircrack-ng {0} -w {1}".format(handshake, wordlist))
+
+    elif option == 9:
+        if os.system("crunch") == "Usage":
+            print("""Crunch can create a wordlist based on criteria you specify.
+            The output from crunch can be sent to the screen, file or another program.
+            
+            Usage: crunch <Min> <Max> <Characters> -t <Pattern> -o <FileName>
+            
+            Example:
+            crunch 6 8 123abc$ -o wordlist -t a@@@@b""")
+
+            min = int(input("Minimum of characters > "))
+            max = int(input("Maximum of characters > "))
+            characters = input("Characters > ")
+            pattern = input("Pattern (Example: a@@@@b) > ")
+            output = str(input("File Name > "))
+
+            os.system("crunch {0} {1} {2} -o {3} -t {4}".format(min, max, characters, pattern, output))
+            print("Your new wordlist saved in {0}".format(output))
+
+        elif not os.system("crunch") == "Usage":
+            print("Installing Crunch{0}".format(enter))
+
+            print("""Crunch can create a wordlist based on criteria you specify.
+The output from crunch can be sent to the screen, file or another program.
+
+Usage: crunch <Min> <Max> <Characters> -t <Pattern> -o <FileName>
+
+Example:
+crunch 6 8 123abc$ -o wordlist -t a@@@@b""")
+
+            min = int(input("Minimum of characters > "))
+            max = int(input("Maximum of characters > "))
+            characters = input("Characters > ")
+            pattern = input("Pattern (Example: a@@@@b) > ")
+            output = str(input("File Name > "))
+
+            os.system("crunch {0} {1} {2} -o {3} -t {4}".format(min, max, characters, pattern, output))
+            print("Your new wordlist saved in {0}".format(output))
+
 
 menu()
