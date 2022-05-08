e0505879712f92d73522280fb71fb5c740a2249e
IdanMalihi <81778053+TraiLeR2@users.noreply.github.com>
2022-05-08 23:40:06 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,7 @@
 import pyfiglet
-from termcolor import colored
 
 enter = "\n"
 
 def banner():
-    bannerr = pyfiglet.figlet_format("WireAttack")
-    print(colored("{0}".format(bannerr), "red"))
+    res = pyfiglet.figlet_format("WireAttack")
+    return res
