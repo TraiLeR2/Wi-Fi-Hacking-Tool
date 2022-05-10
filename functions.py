9cc8c10d76967121e6caa4300d49164914a28c96
IdanMalihi <81778053+TraiLeR2@users.noreply.github.com>
2022-05-10 10:44:28 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,14 @@
 import pyfiglet
+import os
 
 enter = "\n"
 
 def banner():
     res = pyfiglet.figlet_format("WireAttack")
     return res
+
+def is_crunch():
+    installed = os.popen("dpkg -s crunch")
+    for line in installed.readlines():
+        if "Status: install ok installed" in line:
+            return line
